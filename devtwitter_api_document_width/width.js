fbb669613f958e8eca34e8d18f079966319d932a
airtoxin <airtoxin@icloud.com>
2015-02-01 11:29:52 +0900

    Add devtwitter

---
+++ devtwitter_api_document_width/width.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByClassName( 'platform-object' );
+for ( var i = 0; i < elements.length; i++ ) {
+	var element = elements[ i ];
+	element.style.width = '900px';
+}
